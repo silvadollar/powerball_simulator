13fb49a23a067f5db19363563047cd327ba608d9
silvadollar <90071538+SilvaNick@users.noreply.github.com>
2021-11-10 20:09:50 -0500

    Updated to put on my website.

--- powerball_simulator.py
+++ powerball_simulator.py
@@ -48,91 +48,97 @@ def check_ticket(mine, winner):
     return True
 
 
-possibilities = [x for x in range(1, 70)]
-power = [x for x in range(1, 27)]
+def main():
 
-winner = get_winning_ticket(possibilities, power)
-# below is just to test a winner
-# winner = [1, 11, 31, 3, 5, 11]
-winner_power = []
-pulled_winner_power = winner.pop()
-winner_power.append(pulled_winner_power)
+    print(f"Welcome to the Powerball simulator where you can choose to \"buy\" as many tickets as you want. "
+          f"This program will create one winning ticket and check every one of your new tickets to see if you won. "
+          f"Then we will let you know how much you won. Hopefully you get the GRAND PRIZE! \n")
+    possibilities = [x for x in range(1, 70)]
+    power = [x for x in range(1, 27)]
 
-plays = 0
-payout = 0
-payout1mil = 0
-payout50k = 0
-payout100 = 0
-payout7 = 0
-payout4 = 0
-won = False
-
-max_attempts = 50_000_000
-
-while not won:
-    mine = get_my_ticket(possibilities, power)
+    winner = get_winning_ticket(possibilities, power)
     # below is just to test a winner
-    # mine = [1, 31, 3, 11, 5, 11]
-    won = check_ticket(mine[:], winner)
+    # winner = [1, 11, 31, 3, 5, 11]
+    winner_power = []
+    pulled_winner_power = winner.pop()
+    winner_power.append(pulled_winner_power)
 
-    mine_power = []
-    pulled_mine_power = mine.pop()
-    mine_power.append(pulled_mine_power)
+    plays = 0
+    payout = 0
+    payout1mil = 0
+    payout50k = 0
+    payout100 = 0
+    payout7 = 0
+    payout4 = 0
+    won = False
 
-    plays += 1
-    solution = set(mine).intersection(winner)
+    max_attempts = eval(input("How many Powerball tickets would you like to buy: "))
 
-    if plays >= max_attempts:
-        print("Reached max attempts...")
-        print(f"Winning ticket: {winner}:{winner_power}")
-        print(f"You won ${payout:,d}.00 in total.")
-        print(f"You won {payout1mil:,d} $1,000,000 prizes")
-        print(f"You won {payout50k:,d} $50,000 prizes")
-        print(f"You won {payout100:,d} $100 prizes")
-        print(f"You won {payout7:,d} $7 prizes")
-        print(f"You won {payout4:,d} $4 prizes")
-        break
+    while not won:
+        mine = get_my_ticket(possibilities, power)
+        # below is just to test a winner
+        # mine = [1, 31, 3, 11, 5, 11]
+        won = check_ticket(mine[:], winner)
 
-    if won:
-        print("GRAND PRIZE WINNER!!!!")
-        print(f"Your ticket: {mine}:{mine_power}")
-        print(f"Winning ticket: {winner}:{winner_power}")
-        print(f"It took {plays} tries.")
-        print(f"You won ${payout:,d}.00 in total")
-        print(f"You won {payout1mil:,d} $1,000,000 prizes")
-        print(f"You won {payout50k:,d} $50,000 prizes")
-        print(f"You won {payout100:,d} $100 prizes")
-        print(f"You won {payout7:,d} $7 prizes")
-        print(f"You won {payout4:,d} $4.00 prizes")
-    else:
-        # print(f"Your ticket: {mine}:{mine_power}")
-        # print(f"Winning ticket: {winner}:{winner_power}")
-        if len(solution) == 5 and mine_power != winner_power:
-            payout += 1_000_000
-            payout1mil += 1
-        elif len(solution) == 4 and mine_power == winner_power:
-            payout += 50_000
-            payout50k += 1
-        elif len(solution) == 4 and mine_power != winner_power:
-            payout += 100
-            payout100 += 1
-        elif len(solution) == 3 and mine_power == winner_power:
-            payout += 100
-            payout100 += 1
-        elif len(solution) == 3 and mine_power != winner_power:
-            payout += 7
-            payout7 += 1
-        elif len(solution) == 2 and mine_power == winner_power:
-            payout += 7
-            payout7 += 1
-        elif len(solution) == 1 and mine_power == winner_power:
-            payout += 4
-            payout4 += 1
-        elif len(solution) == 2 and mine_power == winner_power:
-            payout += 4
-            payout4 += 1
+        mine_power = []
+        pulled_mine_power = mine.pop()
+        mine_power.append(pulled_mine_power)
+
+        plays += 1
+        solution = set(mine).intersection(winner)
+
+        if plays >= max_attempts:
+            print("\nReached max attempts...")
+            print(f"Winning ticket: {winner}:{winner_power}")
+            print(f"You won ${payout:,d}.00 in total.")
+            print(f"You won {payout1mil:,d} $1,000,000 prizes")
+            print(f"You won {payout50k:,d} $50,000 prizes")
+            print(f"You won {payout100:,d} $100 prizes")
+            print(f"You won {payout7:,d} $7 prizes")
+            print(f"You won {payout4:,d} $4 prizes")
+            break
+
+        if won:
+            print("\nGRAND PRIZE WINNER!!!!")
+            print(f"Your ticket: {mine}:{mine_power}")
+            print(f"Winning ticket: {winner}:{winner_power}")
+            print(f"It took {plays} tries.")
+            print(f"You won ${payout:,d}.00 in total")
+            print(f"You won {payout1mil:,d} $1,000,000 prizes")
+            print(f"You won {payout50k:,d} $50,000 prizes")
+            print(f"You won {payout100:,d} $100 prizes")
+            print(f"You won {payout7:,d} $7 prizes")
+            print(f"You won {payout4:,d} $4.00 prizes")
         else:
-            payout += 0
+            # print(f"Your ticket: {mine}:{mine_power}")
+            # print(f"Winning ticket: {winner}:{winner_power}")
+            if len(solution) == 5 and mine_power != winner_power:
+                payout += 1_000_000
+                payout1mil += 1
+            elif len(solution) == 4 and mine_power == winner_power:
+                payout += 50_000
+                payout50k += 1
+            elif len(solution) == 4 and mine_power != winner_power:
+                payout += 100
+                payout100 += 1
+            elif len(solution) == 3 and mine_power == winner_power:
+                payout += 100
+                payout100 += 1
+            elif len(solution) == 3 and mine_power != winner_power:
+                payout += 7
+                payout7 += 1
+            elif len(solution) == 2 and mine_power == winner_power:
+                payout += 7
+                payout7 += 1
+            elif len(solution) == 1 and mine_power == winner_power:
+                payout += 4
+                payout4 += 1
+            elif len(solution) == 2 and mine_power == winner_power:
+                payout += 4
+                payout4 += 1
+            else:
+                payout += 0
 
 
-
+if __name__ == '__main__':
+    main()
